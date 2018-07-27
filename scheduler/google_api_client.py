1f27c9564e036309ade23c811613ca093d3c63d8
David Guzman <d.guzman@ucl.ac.uk>
2018-07-27 17:08:05 +0100

    Scaffold methods in GoogleApiClient class

--- scheduler/google_api_client.py
+++ scheduler/google_api_client.py
@@ -2,6 +2,38 @@
  Collects data from Google Health Trends API
 """
 
+from datetime import date
+from googleapiclient.discovery import build
+from os import environ
+from typing import List
+
+_DISCOVERY_SERVICE_URL = 'https://www.googleapis.com/discovery/v1/apis/trends/v1beta/rest'
+_GEORESTRICTION_REGION = 'GB-ENG'
+_GOOGLE_API_KEY = environ["GOOGLE_API_KEY"]
+_ISO_FORMAT = '%Y-%m-%d'
+_SERVICE_NAME = 'trends'
+_SERVICE_VERSION = 'v1beta'
+_TIMELINE_RESOLUTION = 'day'
+
 
 class GoogleApiClient(object):
-    pass
+
+    def __init__(self):
+        self.service = build(
+            serviceName=_SERVICE_NAME,
+            version=_SERVICE_VERSION,
+            discoveryServiceUrl=_DISCOVERY_SERVICE_URL,
+            developerKey=_GOOGLE_API_KEY,
+            cache_discovery=False
+        )
+
+    def fetch_google_scores(self, terms:List[str], start:date, end:date):
+        graph = self.service.getTimelinesForHealth(
+            terms=terms,
+            geoRestriction_region=_GEORESTRICTION_REGION,
+            time_startDate=start.strftime(_ISO_FORMAT),
+            time_endDate=end.strftime(_ISO_FORMAT),
+            timelineResolution=_TIMELINE_RESOLUTION
+        )
+        response = graph.execute()
+        pass
--- scheduler/score_calculator.py
+++ scheduler/score_calculator.py
@@ -4,11 +4,16 @@
 
 from datetime import date
 
-from .score_query_registry import get_date_ranges_google_score
+from .google_api_client import GoogleApiClient
+from .score_query_registry import get_date_ranges_google_score, get_google_batch
 
 
 def run(model_id: int, start: date, end: date):
     """ Calculate the model score for the date range specified """
     missing_google_dates = get_date_ranges_google_score(model_id, start, end)
     if missing_google_dates:
+        batch = get_google_batch(model_id, missing_google_dates)
+        api_client = GoogleApiClient()
+        for terms, start_date, end_date in batch:
+            print("TEST")
         pass
