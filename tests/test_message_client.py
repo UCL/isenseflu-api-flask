eaf20702bf09455235f0ce8ebb151951b0d6dad5
David Guzman <d.guzman@ucl.ac.uk>
2018-08-07 14:34:05 +0100

    Unit tests for message_client

---
+++ tests/test_message_client.py
@@ -0,0 +1,38 @@
+"""
+ Tests STOMP messaging client
+"""
+
+from datetime import date
+from unittest import TestCase
+from unittest.mock import patch, Mock
+
+from scheduler.message_client import build_message_client, MessageClient
+
+
+class MessageClientTestCase(TestCase):
+    """ Test case for scheduler.message_client.py """
+
+    def test_build_client(self):
+        """
+        Evaluate creation of an instance of MessageClient
+        """
+        with patch.multiple('scheduler.message_client',
+                            _MQ_URI='tcp://openmqhost:7672',
+                            _MQ_DEST='/queue/PubTest.Q',
+                            _MQ_USER='testuser', _MQ_PASS='testpass'):
+            instance = build_message_client()
+            self.assertIsInstance(instance, MessageClient)
+
+    def test_publish_model_score(self):
+        """
+        Publish a model score and date in the message queue
+        """
+        with patch.multiple('scheduler.message_client',
+                            _MQ_URI='tcp://mock:7672',
+                            _MQ_DEST='/queue/PubTest.Q',
+                            _MQ_USER='testuser', _MQ_PASS='testpass'):
+            instance = build_message_client()
+            instance.client = Mock()
+            instance.client.send = Mock()
+            instance.publish_model_score(date(2018, 1, 1), 1.0)
+            self.assertEqual(instance.client.send.call_count, 1)
