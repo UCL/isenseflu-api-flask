92fa6128031ea9b130aaf1989e080171b5fcdd63
David Guzman <d.guzman@ucl.ac.uk>
2019-02-01 18:17:37 +0000

    WIP: Implement calculation using octave

---
+++ tests/mfiles/model_score_octave.m
@@ -0,0 +1,3 @@
+function y_inf = model_score_octave(terms,scores,result)
+    y_inf = 1.0
+endfunction
---
+++ tests/test_calculator_builder.py
@@ -0,0 +1,25 @@
+import sys
+from os import makedirs
+from pathlib import Path
+from shutil import copy2, rmtree, which
+from unittest import TestCase
+
+from scheduler.calculator_builder import CalculatorType, build_calculator
+
+
+class CalculatorBuilderTestCase(TestCase):
+    """ Test case for module scheduler.calculator_builder """
+
+    def test_calculation_in_matlab(self):
+        if 'matlab' not in sys.modules:
+            pass
+
+    def test_calculation_in_octave(self):
+        if which('octave') is not None:
+            makedirs("octave/gpml")
+            Path("octave/gpml/startup.m").touch()
+            engine = build_calculator(CalculatorType['OCTAVE'])
+            copy2('tests/mfiles/model_score_octave.m', 'octave')
+            val = engine.calculate_model_score('model_score_octave', [('term1', 1.0), ('term2', 2.0)])
+            rmtree("octave")
+            self.assertEqual(val, 1.0)
