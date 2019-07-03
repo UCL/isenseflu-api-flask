27bc2c181ff205eb9d7ecdb7a0522cde48901688
David Guzman <d.guzman@ucl.ac.uk>
2019-07-03 12:41:43 +0100

    Refactoring of method to build response for root, plink and twlink endpoints

--- app/response_template_registry.py
+++ app/response_template_registry.py
@@ -26,6 +26,32 @@ def build_models_and_metadata(
             'name': flu_model.name
         }
         flu_model_list.append(obj)
+    response = {
+        'model_list': flu_model_list,
+        'rate_thresholds': rate_thresholds,
+        'model_data': __build_model_data(model_data)
+    }
+    return response
+
+
+def build_scores_response(model_data: List[Tuple[Dict, List[ModelScore]]]) -> Dict:
+    """
+    Constructs response for a message that contains metadata and scores for a list of flu models
+    :param model_data:
+    :return: a dictionary with the data in serialisable form
+    """
+    response = {
+        'model_data': __build_model_data(model_data)
+    }
+    return response
+
+
+def __build_model_data(model_data: List[Tuple[Dict, List[ModelScore]]]) -> List:
+    """
+    Constructs list of dictionaries containing metadata and scores from flu models
+    :param model_data:
+    :return: a list with the model metadata and scores
+    """
     flu_model_data = []
     for model_data_item, model_scores_item in model_data:
         converted_model_scores = [
@@ -40,9 +66,4 @@ def build_models_and_metadata(
         model_data_item['start_date'] = model_data_item['start_date'].strftime('%Y-%m-%d')
         model_data_item['end_date'] = model_data_item['end_date'].strftime('%Y-%m-%d')
         flu_model_data.append(model_data_item)
-    response = {
-        'model_list': flu_model_list,
-        'rate_thresholds': rate_thresholds,
-        'model_data': flu_model_data
-    }
-    return response
+    return flu_model_data
--- tests/test_response_template_registry.py
+++ tests/test_response_template_registry.py
@@ -6,7 +6,7 @@ from datetime import date
 from unittest import TestCase
 
 from app.models import FluModel, ModelScore
-from app.response_template_registry import build_models_and_metadata
+from app.response_template_registry import build_models_and_metadata, build_scores_response
 
 
 class ResponsesTestCase(TestCase):
@@ -65,3 +65,39 @@ class ResponsesTestCase(TestCase):
         )]
         result = build_models_and_metadata(flu_model_list, rate_thresholds, flu_model_data)
         self.assertEquals(result, expected)
+
+    def test_build_scores_response(self):
+        data_points = [
+            {'score_date': '2019-01-01', 'score_value': 0.2, 'confidence_interval_lower': 0.1,
+             'confidence_interval_upper': 0.5},
+            {'score_date': '2019-01-02', 'score_value': 0.4, 'confidence_interval_lower': 0.1,
+             'confidence_interval_upper': 0.5}
+        ]
+        model_data = [{
+            'id': 1,
+            'name': 'Test Model',
+            'start_date': '2019-01-01',
+            'end_date': '2019-01-02',
+            'average_score': 0.3,
+            'has_confidence_interval': True,
+            'data_points': data_points
+        }]
+        expected = {'model_data': model_data}
+        flu_model_score_1, flu_model_score_2 = ModelScore(), ModelScore()
+        flu_model_score_1.score_date, flu_model_score_2.score_date = date(2019, 1, 1), date(2019, 1, 2)
+        flu_model_score_1.score_value, flu_model_score_2.score_value = 0.2, 0.4
+        flu_model_score_1.confidence_interval_lower, flu_model_score_2.confidence_interval_lower = 0.1, 0.1
+        flu_model_score_1.confidence_interval_upper, flu_model_score_2.confidence_interval_upper = 0.5, 0.5
+        flu_model_data = [(
+            {
+                'id': 1,
+                'name': 'Test Model',
+                'start_date': date(2019, 1, 1),
+                'end_date': date(2019, 1, 2),
+                'has_confidence_interval': True,
+                'average_score': 0.3
+            },
+            [flu_model_score_1, flu_model_score_2]
+        )]
+        result = build_scores_response(flu_model_data)
+        self.assertDictEqual(result, expected)
