1302963a334f4e2fb96a44efeeb290f926a00eb5
David Guzman <d.guzman@ucl.ac.uk>
2019-08-06 12:04:41 +0100

    Add license header

--- app/models_query_registry.py
+++ app/models_query_registry.py
@@ -1,3 +1,21 @@
+# i-sense flu api: REST API, and data processors for the i-sense flu service from UCL.
+# (c) 2019, UCL <https://www.ucl.ac.uk/
+#
+# This file is part of i-sense flu api
+#
+# i-sense flu api is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# i-sense flu api is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with i-sense flu api.  If not, see <http://www.gnu.org/licenses/>.
+
 """
  Registry of data access functions used to retrieve data from the ORM definitions
  in models.py
